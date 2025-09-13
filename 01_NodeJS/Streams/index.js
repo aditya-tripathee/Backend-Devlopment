cfbf3457899a63d622e7fecd1258855996a95c69
Aditya Tripathi <adityatripathieee@gmail.com>
2025-09-13 14:01:20 +0530

    updated streams

--- 01_NodeJS/Streams/index.js
+++ 01_NodeJS/Streams/index.js
@@ -1,1 +1,6 @@
-console.log(`This is the stream file`);
+
+// Stream is something that means data can flow in chunks.
+// A Stream in Node.js means data flows in small chunks instead of loading everything at once.
+
+// A buffer is a temporary physical memory space (in RAM) that stores raw binary data (bytes) before it’s processed.
+
